d459786977bf0b200b061e0e62fa6c8f57c68512
Alexis Borges <alexis.borges@estudiantes.utec.edu.uy>
2025-08-19 20:04:02 -0300

    feat: add implementar lista productos

---
+++ js/products.js
@@ -0,0 +1,34 @@
+let products = [];
+
+
+function showProductsList() {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < products.length; i++) {
+        let product = products[i];
+
+        htmlContentToAppend += `
+            <div class="col-md-4 mb-4">
+              <div class="product-card">
+                <img src="${product.image}" alt="Producto" class="product-image">
+                <h5 class="fw-bold">${product.name}
+                </h5>
+                <p class="text-muted">${product.description}</p>
+                <p class="price"> ${product.currency} ${product.cost}</p>
+                <p class="sold">${product.soldCount} vendidos</p>
+              </div>
+            </div>
+        `
+    }
+    document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+}
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData("https://japceibal.github.io/emercado-api/cats_products/101.json").then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            products = resultObj.data.products;
+            showProductsList();
+        }
+    });
+});
