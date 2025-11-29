9e84d482184a11e2aa03d71ff1ebdf570e957f80
Meewh <meewhdev@gmail.com>
2025-11-29 12:36:15 -0300

    cambios con manu
    para ake

--- backend-mikan/app.js
+++ backend-mikan/app.js
@@ -5,6 +5,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var catsRouter = require('./routes/cats');
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const JWT_SECRET = "curso-jap-secret";
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,6 +32,40 @@ app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/cats', catsRouter);
 
+const users = JSON.parse(fs.readFileSync("./mock_bd/users.json", "utf8"));
+
+app.post("/login", (req, res) => {
+  // Tomamos las credenciales enviadas por el formulario
+  const { username, password } = req.body;
+
+  // Validamos que no vengan vacias
+  if (!username || !password) {
+    return res.status(400).json({ message: "Usuario y contraseña son obligatorios" });
+  }
+
+  // Buscamos el usuario en el JSON cargado al iniciar el servidor
+  const user = users.find((u) => u.username === username);
+  if (!user) {
+    return res.status(401).json({ message: "Credenciales inválidas" });
+  }
+
+  // Comparamos la contraseña en texto con el hash guardado
+  if (password !== user.password) {
+    return res.status(401).json({ message: "Credenciales inválidas" });
+  }
+
+  // Firmamos un token sencillo con el id y username
+  const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, { expiresIn: "2h" });
+
+  res.json({
+    token,
+    user: {
+      id: user.id,
+      username: user.username,
+      fullName: user.fullName,
+    },
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
